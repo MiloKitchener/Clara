b401bbd9538c399847fc423d28a4a67ba6cc829d
William McKinnon <willpower3309@gmail.com>
2019-05-07 11:08:00 -0700

    Added login page functionality

--- ClaraFrontend/src/app/app-routing.module.ts
+++ ClaraFrontend/src/app/app-routing.module.ts
@@ -9,9 +9,8 @@ import { AuthService } from './services/auth/auth.service';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'signin', pathMatch: 'full' },
-  { path: 'signin', component: LoginComponent, data: { title: 'Sign In' } },
-  { path: 'personalDashboard', component: UserDashboardComponent, data: { title: 'Personal Dashboard' }, canActivate: [AuthService] },
+  { path: '', redirectTo: 'personalDashboard', pathMatch: 'full' },
+  { path: 'personalDashboard', component: UserDashboardComponent, data: { title: 'Personal Dashboard' } },
   { path: 'sharedDashboard', component: SharedDashboardComponent, data: { title: 'Shared Dashboards' } },
   { path: 'datasets', component: DatasetsComponent, data: { title: 'Datasets' } },
   { path: 'ideas', component: IdeasComponent, data: { title: 'Ideas' } }
