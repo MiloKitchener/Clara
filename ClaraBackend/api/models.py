8a41d7f13fb231acf503e70ccc46ad07d5dab9af
Lunarelements <bradlabrador@hotmail.com>
2019-05-10 10:57:03 -0400

    Working on dataset fetching and listing

--- ClaraBackend/api/models.py
+++ ClaraBackend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Dataset(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    desc = models.CharField(max_length=255, null=False)
+    api_url = models.CharField(max_length=255, null=False)
+    datetime_updated = models.DateTimeField(max_length=255, null=False)
--- ClaraBackend/api/views.py
+++ ClaraBackend/api/views.py
@@ -1,23 +1,31 @@
-from django.http import HttpResponseRedirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from django.contrib import auth
+from .models import Dataset
+from .serializers import DatasetSerializer
 
 
 class HelloView(APIView):
     permission_classes = (IsAuthenticated,)
 
-    def get(self, request):
+    def get(self):
         content = {'message': 'Hello, World!'}
         return Response(content)
 
 
-def login_view(request):
-    user = auth.authenticate(username=request.POST.get('password', ''), password=request.POST.get('username', ''))
-    if user is not None and user.is_active:
-        # Correct password, and the user is marked "active"
-        auth.login(request, user)
-    else:
-        # Show an error page
-        return HttpResponseRedirect("/api/invalid/")
+class ListDatasetView(generics.ListAPIView):
+    # Authenticate the user
+    # TODO: Re-enable authentication
+    # permission_classes = (IsAuthenticated,)
+
+    # Select all datasets
+    queryset = Dataset.objects.all()
+    serializer_class = DatasetSerializer
+
+
+def graph(request):
+    dataset1 = request.POST.get('dataset1', '')
+    field1 = request.POST.get('field1', '')
+    dataset2 = request.POST.get('dataset2', '')
+    field2 = request.POST.get('field2', '')
