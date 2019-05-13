d6c3083fdf20196c43822beeaecad0350675b514
Lunarelements <bradlabrador@hotmail.com>
2019-05-13 16:15:43 -0400

    Fixed frontend chart problems

--- ClaraFrontend/src/app/components/graph-panel/graph-panel.component.ts
+++ ClaraFrontend/src/app/components/graph-panel/graph-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-graph-panel',
@@ -7,36 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphPanelComponent implements OnInit {
 
-  private graphTitle = "";
-  private yAxisTitle = "";
-  private xAxisTitle = "";
+  private graphTitle = '';
+  private yAxisTitle = '';
+  private xAxisTitle = '';
   private yFields = null;
   private xFields = null;
 
   private datasets = [
-    {"title": "Temperature"},
-    {"title": "Traffic"},
-    {"title": "Weather"},
-    {"title": "Wind Speed"}
-  ]
+    {title: 'Temperature'},
+    {title: 'Traffic'},
+    {title: 'Weather'},
+    {title: 'Wind Speed'}
+  ];
   private fields = [
-    {"dataset":"Temperature", "fields":["Temperature Field A"]},
-    {"dataset":"Traffic", "fields":["Traffic Field A", "Traffic Field B"]},
-    {"dataset":"Weather", "fields":["Weather Field A", "Weather Field B", "Weather Field C"]},
-    {"dataset":"Wind Speed", "fields":["Wind Speed Field A", "Wind Speed Field B", "Wind Speed Field C"]}
-  ]
+    {dataset: 'Temperature', fields: ['Temperature Field A']},
+    {dataset: 'Traffic', fields: ['Traffic Field A', 'Traffic Field B']},
+    {dataset: 'Weather', fields: ['Weather Field A', 'Weather Field B', 'Weather Field C']},
+    {dataset: 'Wind Speed', fields: ['Wind Speed Field A', 'Wind Speed Field B', 'Wind Speed Field C']}
+  ];
 
   constructor() {
-    this.yAxisTitle = "None";
-    this.xAxisTitle = "None";
+    this.yAxisTitle = 'None';
+    this.xAxisTitle = 'None';
   }
 
   ngOnInit() {
-    this.graphTitle = "Y-Axis V X-Axis";
+    this.graphTitle = 'Y-Axis V X-Axis';
 
     // Chart Script
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    const canvas: any = document.getElementById('myChart');
+    const ctx = canvas.getContext('2d');
+    const myChart = new Chart(ctx, {
       type: 'bar',
       data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -76,44 +78,43 @@ export class GraphPanelComponent implements OnInit {
 
   setYAxis(title) {
     this.yAxisTitle = title;
-    this.graphTitle = this.yAxisTitle + " V " + this.xAxisTitle;
+    this.graphTitle = this.yAxisTitle + ' V ' + this.xAxisTitle;
 
     // get fields
-    var found = 0;
-    for(var field in this.fields) {
+    const found = 0;
+    for (const field in this.fields) {
       /*if(field.dataset == title){
         found = 1;
         break;
       }*/
     }
-    if(found == 0) {
-      alert("No Fields Found Corresponding to Dataset " + title);
+    if (found === 0) {
+      alert('No Fields Found Corresponding to Dataset ' + title);
     }
   }
 
   setXAxis(title) {
     this.xAxisTitle = title;
-    this.graphTitle = this.yAxisTitle + " V " + this.xAxisTitle;
+    this.graphTitle = this.yAxisTitle + ' V ' + this.xAxisTitle;
 
     // get fields
-    var found = 0;
-    for(var field in this.fields) {
+    const found = 0;
+    for (const field in this.fields) {
       /*if(field.dataset == title){
         found = 1;
         break;
       }*/
     }
-    if(found == 0) {
-      alert("No Fields Found Corresponding to Dataset " + title);
+    if (found === 0) {
+      alert('No Fields Found Corresponding to Dataset ' + title);
     }
   }
 
   addGraph() {
-    if(this.yAxisTitle == "None" || this.xAxisTitle == "None") {
-      alert("Please Specify X / Y Axis Values");
-    }
-    else {
-      alert("added graph");
+    if (this.yAxisTitle === 'None' || this.xAxisTitle === 'None') {
+      alert('Please Specify X / Y Axis Values');
+    } else {
+      alert('added graph');
     }
   }
 }
