c2cc51a7231226f9f3c14bf64b2951a7b5874d71
William McKinnon <willpower3309@gmail.com>
2019-05-16 11:20:59 -0700

    Added Logout Functionality

--- ClaraFrontend/src/app/main/main.component.ts
+++ ClaraFrontend/src/app/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'src/app/services/auth/auth.service';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -14,8 +16,16 @@ export class MainComponent implements OnInit {
     "pictureSrc": "assets/images/avatar.png"
   }
 
-  constructor() { }
+  constructor(
+    private logoutService: AuthService
+  ) { }
 
   ngOnInit() { }
 
+  // logs user out of the site, returns to login page
+  logout() {
+    this.logoutService.logout();
+    // Reload the current page without the browser cache
+    location.reload(true);
+  }
 }
