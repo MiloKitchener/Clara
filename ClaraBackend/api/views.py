e61ed3b85cd580e18d0b15b0e73eb231f190fc52
Jake-Vink <vinkj@uoguelph.ca>
2019-05-17 11:41:55 -0400

    added fuzzy matching options to pesudo-standardize database fields on input

--- ClaraBackend/api/views.py
+++ ClaraBackend/api/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .forms import CustomUserCreationForm
 from rest_framework import viewsets
 import json
-from .dal import fetch_data, combine_data_list, create_datasets
+from .dal import *
 from rest_framework.decorators import action
 
 
@@ -55,6 +55,8 @@ class DatasetCreateView(APIView):
     def post(self, request):
         url = request.data.get('url')
         print(url)
+        # for jakes test's
+       # map_fields_to_normalized_name(url)
         create_datasets(url)
         return Response("Success")
 
