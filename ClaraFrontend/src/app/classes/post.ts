aa8e1a6ab95ab2896bf392723ec96ef0c15dabd0
WillPower3309 <willpower3309@gmail.com>
2019-05-30 16:29:28 -0400

    Improved Ideas UI

---
+++ ClaraFrontend/src/app/classes/post.ts
@@ -0,0 +1,9 @@
+export class Post {
+	title: string;
+	description: string;
+	numComments: number;
+	numVotes: number;
+	authorName: string;
+	imgSrc: string;
+	tag: string;
+}
--- ClaraFrontend/src/app/components/idea-node/idea-node.component.ts
+++ ClaraFrontend/src/app/components/idea-node/idea-node.component.ts
@@ -23,11 +23,11 @@ export class IdeaNodeComponent implements OnInit {
   }
 
   upvote() {
-    if(this.voted == 0) {
+    if (this.voted == 0) {
       this.numVotes++;
       this.voted = 1;
     }
-    else if(this.voted == 2) { // if previously downvoted, add two to remove downvote and add upvote
+    else if (this.voted == 2) { // if previously downvoted, add two to remove downvote and add upvote
       this.numVotes += 2;
       this.voted = 1;
     }
@@ -38,11 +38,11 @@ export class IdeaNodeComponent implements OnInit {
   }
 
   downvote() {
-    if(this.voted == 0) {
+    if (this.voted == 0) {
       this.numVotes--;
       this.voted = 2;
     }
-    else if(this.voted == 1) { // if previously upvoted, sub two to remove upvote and add downvote
+    else if (this.voted == 1) { // if previously upvoted, sub two to remove upvote and add downvote
       this.numVotes -= 2;
       this.voted = 2;
     }
@@ -50,5 +50,10 @@ export class IdeaNodeComponent implements OnInit {
       this.numVotes++;
       this.voted = 0;
     }
+  }
+
+  viewComments() {
+    document.getElementById("commentPanel").style.animation = "fadein 2s";;
+    document.getElementById("commentPanel").style.display = "block";
   }
 }
