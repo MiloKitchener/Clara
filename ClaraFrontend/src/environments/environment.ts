bfebd9ad891e6fda7e281d93f3582bd90db9f372
WillPower3309 <willpower3309@gmail.com>
2019-06-05 16:43:36 -0400

    Fixed navbar hovering issues

--- ClaraFrontend/src/environments/environment.ts
+++ ClaraFrontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   backendIP: 'http://35.182.196.173/',
   access_token_name: 'access_token',
   refresh_token_name: 'refresh_token',
