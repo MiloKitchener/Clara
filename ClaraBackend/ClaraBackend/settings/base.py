ed6bc41b20ac80b94f30bd8106363f258815ff27
Lunarelements <bradlabrador@hotmail.com>
2019-06-12 14:27:23 -0400

    Oops

--- ClaraBackend/ClaraBackend/settings/base.py
+++ ClaraBackend/ClaraBackend/settings/base.py
@@ -82,10 +82,10 @@ ASGI_APPLICATION = "ClaraBackend.routing.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clara',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',  # IP Address that the DB is hosted on
+        'NAME': 'clara_temp',
+        'USER': 'Clara',
+        'PASSWORD': 'T1meMachine',
+        'HOST': 'prod-clarabackenddb.clhelwr0pylt.ca-central-1.rds.amazonaws.com',  # IP Address that the DB is hosted on
         'PORT': '3306',
     }
 }
