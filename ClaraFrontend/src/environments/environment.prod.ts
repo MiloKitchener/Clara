001e990e6bd196979a24539fc9249d2ac3cbb44f
Lunarelements <bradlabrador@hotmail.com>
2019-06-25 09:53:21 -0400

    Environment variables

--- ClaraFrontend/src/environments/environment.prod.ts
+++ ClaraFrontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  backendIP: 'http://localhost:8000/',
+  backendIP: 'http://35.182.196.173/',
+  backendIPWS: 'ws://35.182.196.173/',
   access_token_name: 'access_token',
   refresh_token_name: 'refresh_token',
 };
