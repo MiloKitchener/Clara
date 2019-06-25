7126d8488c3f078698b928b02d01a17923211135
Lunarelements <bradlabrador@hotmail.com>
2019-06-25 11:03:08 -0400

    Removed async

--- ClaraBackend/api/consumers.py
+++ ClaraBackend/api/consumers.py
@@ -1,28 +1,28 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 import websockets
 import json
 
 
-class ClaraConsumer(AsyncWebsocketConsumer):
+class ClaraConsumer(WebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.websocket = None
 
-    async def connect(self):
-        await self.accept()
+    def connect(self):
+        self.accept()
 
-    async def disconnect(self, close_code):
-        await self.websocket.close()
-        await self.close()
+    def disconnect(self, close_code):
+        self.websocket.close()
+        self.close()
 
-    async def receive(self, text_data=None, bytes_data=None):
+    def receive(self, text_data=None, bytes_data=None):
         json_text_data = json.loads(text_data)
-        self.websocket = await websockets.connect(json_text_data['datasets']['api_url'])
-        await self.websocket.send(text_data)
-        await self.receive_websocket()
+        self.websocket = websockets.connect(json_text_data['datasets']['api_url'])
+        self.websocket.send(text_data)
+        self.receive_websocket()
 
-    async def send_websocket(self, message):
-        await self.send(message)
+    def send_websocket(self, message):
+        self.send(message)
 
     async def receive_websocket(self):
         while True:
