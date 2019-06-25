10f1887f5e025687a8a4a3c0e1adaad3bdf01764
Lunarelements <bradlabrador@hotmail.com>
2019-06-25 10:59:50 -0400

    Possible ws fix

--- ClaraBackend/api/consumers.py
+++ ClaraBackend/api/consumers.py
@@ -6,7 +6,7 @@ import json
 class ClaraConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.websocket = websockets.WebSocketClientProtocol
+        self.websocket = None
 
     async def connect(self):
         await self.accept()
@@ -18,12 +18,12 @@ class ClaraConsumer(AsyncWebsocketConsumer):
         json_text_data = json.loads(text_data)
         self.websocket = await websockets.connect(json_text_data['datasets']['api_url'])
         await self.websocket.send(text_data)
-        await self.receive_websocket(ws=self.websocket)
+        await self.receive_websocket()
 
     async def send_websocket(self, message):
         await self.send(message)
 
-    async def receive_websocket(self, ws):
+    async def receive_websocket(self):
         while True:
-            async for message in ws:
+            async for message in self.websocket:
                 await self.send_websocket(message=message)
