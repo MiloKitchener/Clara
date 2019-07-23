1dae48d944969d142a640c8daa841a668928f304
William McKinnon <willpower3309@gmail.com>
2019-07-23 16:23:04 -0400

    added main chart

--- ClaraFrontend/src/app/classes/dashboard.ts
+++ ClaraFrontend/src/app/classes/dashboard.ts
@@ -2,5 +2,6 @@ import { Chart } from './chart';
 
 export class Dashboard {
   name: string;
+  mainChart: Chart;
   charts: Chart[];
 }
--- ClaraFrontend/src/app/components/dashboards/dashboard/dashboard.component.ts
+++ ClaraFrontend/src/app/components/dashboards/dashboard/dashboard.component.ts
@@ -34,6 +34,11 @@ export class DashboardComponent implements OnInit {
     // GET Dashboard
     var sampleJSONGetRes = {
       name: this.name,
+      mainChart: {
+        type: "bar",
+        labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+        data: [{data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}, {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}]
+      },
       charts: [ {
           type: "bar",
           labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
