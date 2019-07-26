0d677afa2c0ab44b1dd52bbf9e905aab07822acf
William McKinnon <willpower3309@gmail.com>
2019-07-26 11:24:10 -0400

    Update main.component.ts

--- ClaraFrontend/src/app/components/main/main.component.ts
+++ ClaraFrontend/src/app/components/main/main.component.ts
@@ -40,49 +40,43 @@ export class MainComponent implements OnInit {
 
   // toggles fullscreen
   public toggleFullScreen(): void {
-    if(this.screenIcon == "fullscreen") {
-      this.openfullscreen();
-    }
-    else {
-      this.closefullscreen();
-    }
-  }
-
-  openfullscreen(): void {
     // Trigger fullscreen
-    const docElmWithBrowsersFullScreenFunctions = document.documentElement as HTMLElement & {
-      mozRequestFullScreen(): Promise<void>;
-      webkitRequestFullscreen(): Promise<void>;
-      msRequestFullscreen(): Promise<void>;
-    };
-  
-    if (docElmWithBrowsersFullScreenFunctions.requestFullscreen) {
-      docElmWithBrowsersFullScreenFunctions.requestFullscreen();
-    } else if (docElmWithBrowsersFullScreenFunctions.mozRequestFullScreen) { /* Firefox */
-      docElmWithBrowsersFullScreenFunctions.mozRequestFullScreen();
-    } else if (docElmWithBrowsersFullScreenFunctions.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-      docElmWithBrowsersFullScreenFunctions.webkitRequestFullscreen();
-    } else if (docElmWithBrowsersFullScreenFunctions.msRequestFullscreen) { /* IE/Edge */
-      docElmWithBrowsersFullScreenFunctions.msRequestFullscreen();
+    if(this.screenIcon == "fullscreen") {
+      const docElmWithBrowsersFullScreenFunctions = document.documentElement as HTMLElement & {
+        mozRequestFullScreen(): Promise<void>;
+        webkitRequestFullscreen(): Promise<void>;
+        msRequestFullscreen(): Promise<void>;
+      };
+    
+      if (docElmWithBrowsersFullScreenFunctions.requestFullscreen) {
+        docElmWithBrowsersFullScreenFunctions.requestFullscreen();
+      } else if (docElmWithBrowsersFullScreenFunctions.mozRequestFullScreen) { /* Firefox */
+        docElmWithBrowsersFullScreenFunctions.mozRequestFullScreen();
+      } else if (docElmWithBrowsersFullScreenFunctions.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        docElmWithBrowsersFullScreenFunctions.webkitRequestFullscreen();
+      } else if (docElmWithBrowsersFullScreenFunctions.msRequestFullscreen) { /* IE/Edge */
+        docElmWithBrowsersFullScreenFunctions.msRequestFullscreen();
+      }
+      this.screenIcon = "fullscreen_exit";
     }
-    this.screenIcon = "fullscreen_exit";
-  }
-  
-  closefullscreen(): void {
-    const docWithBrowsersExitFunctions = document as Document & {
-      mozCancelFullScreen(): Promise<void>;
-      webkitExitFullscreen(): Promise<void>;
-      msExitFullscreen(): Promise<void>;
-    };
-    if (docWithBrowsersExitFunctions.exitFullscreen) {
-      docWithBrowsersExitFunctions.exitFullscreen();
-    } else if (docWithBrowsersExitFunctions.mozCancelFullScreen) { /* Firefox */
-      docWithBrowsersExitFunctions.mozCancelFullScreen();
-    } else if (docWithBrowsersExitFunctions.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-      docWithBrowsersExitFunctions.webkitExitFullscreen();
-    } else if (docWithBrowsersExitFunctions.msExitFullscreen) { /* IE/Edge */
-      docWithBrowsersExitFunctions.msExitFullscreen();
+
+    // close fullscreen
+    else {
+      const docWithBrowsersExitFunctions = document as Document & {
+        mozCancelFullScreen(): Promise<void>;
+        webkitExitFullscreen(): Promise<void>;
+        msExitFullscreen(): Promise<void>;
+      };
+      if (docWithBrowsersExitFunctions.exitFullscreen) {
+        docWithBrowsersExitFunctions.exitFullscreen();
+      } else if (docWithBrowsersExitFunctions.mozCancelFullScreen) { /* Firefox */
+        docWithBrowsersExitFunctions.mozCancelFullScreen();
+      } else if (docWithBrowsersExitFunctions.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        docWithBrowsersExitFunctions.webkitExitFullscreen();
+      } else if (docWithBrowsersExitFunctions.msExitFullscreen) { /* IE/Edge */
+        docWithBrowsersExitFunctions.msExitFullscreen();
+      }
+      this.screenIcon = "fullscreen";
     }
-    this.screenIcon = "fullscreen";
   }
 }
