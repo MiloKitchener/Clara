d4f5ce55d54f0a5c1f6b96799493f46b3814d436
willpower3309 <willpower3309@gmail.com>
2019-07-30 10:44:54 +0000

    Fixed dashboard spacing / sizing

--- ClaraFrontend/src/app/components/devices/devices.component.ts
+++ ClaraFrontend/src/app/components/devices/devices.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+// import classes
+import { Device } from 'src/app/classes/device';
+
 @Component({
   selector: 'app-devices',
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.scss']
 })
 export class DevicesComponent implements OnInit {
+  devices: Device[];
 
   constructor() { }
 
   ngOnInit() {
+    this.devices = [
+      {
+        name: "Clara Mobile 1",
+        sensors: ["Soil Moisture", "Humidity"]
+      }, {
+        name: "Clara Hub 1",
+        sensors: ["Co2", "UV", "Temperature"]
+      }
+    ];
   }
 
 }
