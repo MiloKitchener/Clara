7089a1d08b6439a885cf13f895b5528d3da30876
William McKinnon <willpower3309@gmail.com>
2019-08-06 12:04:37 -0400

    Made clara electron friendly

---
+++ ClaraFrontend/main.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // load the dist folder from Angular
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/claraFrontend/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.removeMenu()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
