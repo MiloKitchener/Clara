c93f8cd65bdc2120acdf3ad60a39b70b2fe4f33e
William McKinnon <willpower3309@gmail.com>
2019-08-16 13:17:13 -0400

    Update dashboard.service.ts

--- ClaraFrontend/src/app/services/dashboard/dashboard.service.ts
+++ ClaraFrontend/src/app/services/dashboard/dashboard.service.ts
@@ -1,10 +1,8 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
-
-// import classes
+import { HttpClient } from '@angular/common/http';
 import { Dashboard } from 'src/app/classes/dashboard';
 import { environment } from './../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -21,24 +19,21 @@ export class DashboardService {
     this.datasets = this.http.get(environment.backendIP + 'datasets/');
   }
 
-
-  // replace?
+  // GET specific dashboard from list
+  // replace with function in dashboard.component.ts?
   getDashboard(dashboards: Dashboard[], dashboardName: string): Dashboard {
     return dashboards.find(o => o.name === dashboardName);
   }
-
 
   // GET Dashboards
   getDashboards() {
     return this.http.get<Dashboard[]>(environment.backendIP + 'dashboards/');
   }
 
-
   // GET the datapoints from a chart specified in the parameters
   getData(params: any) {
     return this.http.post<[]>(environment.backendIP + 'graphs/request_graph/', params);
   }
-
 
   // moves a chart from one dashboard to another
   moveChartToDashboard(dashboards: Dashboard[], originDashboard: Dashboard, dashboardIndex: number, chartIndex: number) {
@@ -47,7 +42,6 @@ export class DashboardService {
     dashboards[dashboardIndex].charts.push(chart);
     // TODO: push changes to database
   }
-
 
   // adds a dashboard
   addDashboard(dashboards: Dashboard[], name: string) {
@@ -62,7 +56,6 @@ export class DashboardService {
     }
   }
 
-
   // Remove dashboard
   removeDashboard(dashboards: Dashboard[], dashboard: Dashboard): void {
     dashboards.splice(dashboards.indexOf(dashboard), 1);
@@ -71,40 +64,37 @@ export class DashboardService {
     // TODO: push changes to database
   }
 
-
   // Returns a list of datasets from the database
   getDatasets() {
     return this.datasets;
   }
-
 
   // GET live datasets
   getLiveDatasets() {
     return this.http.get<any>(environment.backendIP + 'datasets/?type=live');
   }
 
-
   // Returns a list of fields corresponding to a database parameter
   getFields(id: string) {
     return this.http.get(environment.backendIP + 'datasets/' + id + '/field_names/');
   }
-
 
   // GET live dataset fields
   getLiveFields(url: string) {
     return this.http.get(url + 'matrix_info/get_fields/');
   }
 
-
   // GET live devices
   getLiveDevices(url: string) {
     return this.http.get(url + 'devices/');
   }
-
 
   // emits message to hide graph panel
   closeGraphPanel() {
     this.closePanel.emit();
   }
 
+  addChart() {
+    alert('adding chart');
+  }
 }
