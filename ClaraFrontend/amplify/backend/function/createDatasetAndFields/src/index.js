e497ada64964b278140f7a051e5b20d72321338a
unknown <muhammad.rizvi@kitchener.ca>
2019-11-04 15:45:44 -0500

    Automatically create Dataset and corresponding fields from URL

--- ClaraFrontend/amplify/backend/function/createDatasetAndFields/src/index.js
+++ ClaraFrontend/amplify/backend/function/createDatasetAndFields/src/index.js
@@ -24,17 +24,12 @@ exports.handler = async function (event, context) {
   let fields = [];
   let name = '';
   await axios.get(url + '?f=json').then((response) => {
-    console.log(response);
-    response.data.fields.forEach((field) => {
-      fields.push(field.name);
-      console.log(field.name);
-    });
-    console.log(fields);
+    name = response.data.name;
+    fields = response.data.fields;
   });
-  console.log('post post');
 
-  const req = new AWS.HttpRequest(appsyncUrl, region);
-  const statement = `mutation CreateDataset($input: CreateDatasetInput!) {
+  // Update the database to contain our new Dataset and get the ID assigned to it
+  const datasetMutation = `mutation CreateDataset($input: CreateDatasetInput!) {
         createDataset(input: $input) {
           id
           name
@@ -45,37 +40,80 @@ exports.handler = async function (event, context) {
         }
       }`;
 
+  let req = buildRequest(datasetMutation, "CreateDataset", {'name': name, 'api_url': url});
+  let signer = new AWS.Signers.V4(req, "appsync", true);
+  signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
+  const datasetData = await new Promise((resolve, reject) => {
+    const httpRequest = https.request({ ...req, host: endpoint }, (result) => {
+      result.on('data', (data) => {
+        data = JSON.parse(data.toString());
+        console.log(data);
+        resolve(data);
+      });
+    });
+    httpRequest.write(req.body);
+    httpRequest.end();
+  });
+  let datasetID = datasetData.data.createDataset.id;
+
+  // Update database to contain the Dataset's associated fields
+  const fieldMutation = `mutation CreateField($input: CreateFieldInput!) {
+        createField(input: $input) {
+          id
+          name
+          normalized_name
+          alias
+          type
+        }
+      }`;
+  let fieldsData = [];
+  // Publish all fields of the Dataset using the corresponding ID
+  for (let i = 0; i < fields.length; ++i) {
+    let field = fields[i];
+   req = buildRequest(fieldMutation, "CreateField",
+    {
+      'name': field.name,
+      'normalized_name': field.name,
+      'alias': field.alias,
+      'type': field.type,
+      'fieldDatasetId': datasetID
+    });
+  signer = new AWS.Signers.V4(req, "appsync", true);
+  signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
+  const data = await new Promise((resolve, reject) => {
+    const httpRequest = https.request({ ...req, host: endpoint }, (result) => {
+      result.on('data', (data) => {
+        data = JSON.parse(data.toString());
+        resolve(data);
+      });
+    });
+    httpRequest.write(req.body);
+    httpRequest.end();
+  });
+  fieldsData.push(data);
+  }
+
+  return {
+    statusCode: 200,
+    body: {
+      datasetData: datasetData,
+      fieldsData: fieldsData
+    }
+  };
+};
+
+
+// Build a POST request to make a GraphQL query. Used to create a new Dataset and its Fields.
+function buildRequest(query, operation, input) {
+  const req = new AWS.HttpRequest(appsyncUrl, region);
   req.method = "POST";
   req.headers.host = endpoint;
   req.headers["Content-Type"] = "application/json";
   req.body = JSON.stringify({
-    query: statement,
-    operationName: "CreateDataset",
-    variables: {input: {
-        'name': name,
-        'api_url': url
-      }}
+    query: query,
+    operationName: operation,
+    variables: { input: input}
   });
 
-  const signer = new AWS.Signers.V4(req, "appsync", true);
-  signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
-
-  const data = await new Promise((resolve, reject) => {
-    const httpRequest = https.request({ ...req, host: endpoint }, (result) => {
-      result.on('data', (data) => {
-        console.log(JSON.parse(data.toString()));
-        resolve(JSON.parse(data.toString()));
-      });
-    });
-
-    httpRequest.write(req.body);
-    httpRequest.end();
-  });
-
-  return {
-    statusCode: 200,
-    // body: data
-    body: 'tempBody'
-  };
-};
-
+  return req;
+}
